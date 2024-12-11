a0ae45bb3cfd3e86fa0469969cdbc83c5ca16c6c
Xiaotong liu <liuxiaotong@bistu.edu.cn>
2024-12-12 07:04:12 +0800

    add AlchemBERT results

---
+++ benchmarks/matbench_v0.1_AlchemBERT/readme.py
@@ -0,0 +1,12 @@
+"""
+This script contains code for AlchemBERT.
+Our code is hosted at https://gitee.com/liuxiaotong15/alchemBERT.
+
+If needed, you can contact us to obtain the trained models for each task 
+and each fold, totaling 25GB.
+"""
+
+if __name__ == "__main__":
+    print("Our code is hosted at https://gitee.com/liuxiaotong15/alchemBERT.")
+    print("If needed, you can contact us to obtain the pre-trained models for each task and each fold, totaling 25GB.")
+
